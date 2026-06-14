fab46d2e7b333182b899ae9692208c0660c5d453
Adil Sukumar <adilsukumar24@gmail.com>
2026-06-15 01:13:23 +0530

    Implement valid parentheses checker in C++

---
+++ LeetCode/Easy/20.Valid_Paranthesis.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mp = {
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'}
+        }; 
+        for(char c : s){
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
+            }
+            else if(c == ')' || c == ']' || c == '}'){
+                if(st.empty() == true){
+                    return false;
+                }
+                else if(c == mp[st.top()]){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();    
+    }
+};
