046f861a4ba017740a97e5766cbb92992fbe88fd
Adil Sukumar <adilsukumar24@gmail.com>
2026-09-11 17:00:54 +0530

    attempts: 11 Sep 2026

---
+++ Attempts/CodeChef/Unknown-Date/1356177783.CANDYDIST.cpp
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Submission: 1356177783
+ * Problem: CANDYDIST
+ * Verdict: wrong answer
+ * Date: Unknown-Date
+ * URL: https://www.codechef.com/problems/CANDYDIST
+ *  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+}
