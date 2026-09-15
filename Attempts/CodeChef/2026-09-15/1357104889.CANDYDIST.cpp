83ddc5462cc438b545e1edb6737c2b9c02ac8dd4
Adil Sukumar <adilsukumar24@gmail.com>
2026-09-15 23:51:59 +0530

    attempts: 15 Sep 2026

---
+++ Attempts/CodeChef/2026-09-15/1357104889.CANDYDIST.cpp
@@ -0,0 +1,28 @@
+/*
+ * Platform: CodeChef
+ * Submission: 1357104889
+ * Problem: CANDYDIST
+ * Verdict: partially accepted
+ * Date: 2026-09-15
+ * URL: https://www.codechef.com/problems/CANDYDIST
+ *  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T, N, M;
+	cin >> T;
+	
+	for(int i = 0; i < T; i++){
+	    cin >> N >> M;
+	    if(N%2 == 0 && M%2 == 0 || M == 1){
+	        cout << "Yes\n";
+	    }
+	    else{
+	        cout << "No\n";
+	    }
+	}
+	return 0;
+}
