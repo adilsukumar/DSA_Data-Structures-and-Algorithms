4a3ad902b3c1c8907334b86fff07b18e83c12300
Adil Sukumar <adilsukumar24@gmail.com>
2026-09-17 23:45:39 +0530

    attempts: 17 Sep 2026

---
+++ Attempts/CodeChef/Unknown-Date/1358768111.EZSPEAK.cpp
@@ -0,0 +1,17 @@
+/*
+ * Platform: CodeChef
+ * Submission: 1358768111
+ * Problem: EZSPEAK
+ * Verdict: wrong answer
+ * Date: Unknown-Date
+ * URL: https://www.codechef.com/problems/EZSPEAK
+ *  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+
+}
---
+++ Attempts/CodeChef/Unknown-Date/1358774833.EZSPEAK.cpp
@@ -0,0 +1,24 @@
+/*
+ * Platform: CodeChef
+ * Submission: 1358774833
+ * Problem: EZSPEAK
+ * Verdict: wrong answer
+ * Date: Unknown-Date
+ * URL: https://www.codechef.com/problems/EZSPEAK
+ *  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string S;
+	int N, T;
+	cin >> T;
+	
+	for(int i = 0; i < T; i++){
+	    cin >> N;
+	    
+	}
+
+}
