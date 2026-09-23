f6c7fc576a84f010a53aea14bb78c7093686dca9
Adil Sukumar <adilsukumar24@gmail.com>
2026-09-23 23:45:14 +0530

    attempts: 23 Sep 2026

---
+++ Attempts/LeetCode/2026-09-23/2151245015.Two_Sum.cpp
@@ -0,0 +1,22 @@
+/*
+ * Platform: LeetCode
+ * Submission: 2151245015
+ * Problem: Two Sum
+ * Verdict: Compile Error
+ * Date: 2026-09-23
+ * URL: https://leetcode.com/problems/two-sum/
+ *  */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 1; j <= i; j++){
+                if(nums[i] + nums[j] == target){
+                    return [i,j];
+                }
+            }
+        }
+        return 0;
+    }
+};
---
+++ Attempts/LeetCode/2026-09-23/2151247396.Two_Sum.cpp
@@ -0,0 +1,22 @@
+/*
+ * Platform: LeetCode
+ * Submission: 2151247396
+ * Problem: Two Sum
+ * Verdict: Wrong Answer
+ * Date: 2026-09-23
+ * URL: https://leetcode.com/problems/two-sum/
+ *  */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 1; j <= i; j++){
+                if(nums[i] + nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
